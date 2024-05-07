d95d6d2e4d150e0efab4b195bc8fb4219bcffd2e
DuaYaseen <duayaseenvadal@gmail.com>
2024-05-07 22:40:40 +0500

    Java script assignment

---
+++ app.js
@@ -0,0 +1,35 @@
+
+// var userName
+// var myName="Dua yaseen";
+// console.log(myName);
+
+// var tittled
+// var message="Hello World";
+// alert(message);
+
+// var data
+// var bioData="John Doe";
+// alert(bioData);
+
+// var bioData="15 years";
+// alert(bioData);
+
+// var border="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+// alert(border);
+
+// document.write("Yeah! I can write something through Java Script");
+// alert("Pizza")
+
+//var age="Im 17 years old";
+// alert(age)
+
+// var time="You have visited this site 14 times";
+// alert(time);
+// document.write("My birth year is 2006");
+// document.write(age);
+
+// var visitorName = "john doe";
+// var productTittle = "XYZ chlothings store"
+// var productQuantity = "5T-shirts (s)"
+// document.write(visitorName + "ordered" + productQuantity + "on" + productTittle)
+
