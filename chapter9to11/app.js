6a44362174bd461ede11261f3059ba8a0959d859
DuaYaseen <duayaseenvadal@gmail.com>
2024-05-21 20:22:03 +0500

    Chapter 9 to 11

---
+++ chapter9to11/app.js
@@ -0,0 +1,24 @@
+// var city = prompt("Please Enter Your City");
+// if(city){
+//     alert("Welcome To The City Of Lights")
+// }
+// var gender="Male"
+//  prompt("Enter Your Gender" )
+// if(gender){
+//     alert("Good Morning Sir")
+// }
+// else{
+//     alert("Good Morning Mam")
+// }
+// prompt("Enter the colour of signal")
+// var color="Yellow"
+ 
+// if(color){
+//     alert("Ready to move")
+// }
+// else if (color==="Red") {
+//     alert("Must Stop")
+// }
+// else{
+//     alert("Move Now")
+// }
